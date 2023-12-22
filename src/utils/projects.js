0f5c6c98faf4d6e776f1f7477aaeae98686d89f1
TomasRullan <tomasrullan@yahoo.com>
2023-12-22 17:05:40 -0500

    utils folder and files complete

---
+++ src/utils/projects.js
@@ -0,0 +1,53 @@
+import ecommers from '../components/images/ecommers.png';
+import socialnetwork from '../components/images/socialnetwork.png';
+import weatherdashboard from '../components/images/weatherdashboard.png';
+import notetaker from '../components/images/notetaker.png';
+import  workdayscheduler from '../components/images/workdayscheduler.png';
+import blogpost from '../components/images/blogpost.png';
+
+const projects = [
+    {
+        id: 1, 
+        name: 'Work day scheduler',
+        image: workdayscheduler,
+        repoLink: 'https://github.com/TomasRullan/-Work-Day-Scheduler.git',
+        siteLink: 'https://tomasrullan.github.io/-Work-Day-Scheduler/'
+    },
+    {
+        id: 2,
+        name: 'social network api',
+        image: socialnetwork,
+        repoLink: 'https://github.com/TomasRullan/Social-Network-API.git',
+        siteLink: 'https://tomasrullan.github.io/Social-Network-API/'
+    },
+    {
+        id: 3,
+        name: 'weather dashboard',
+        image: weatherdashboard,
+        repoLink: 'https://github.com/TomasRullan/Weather-Dashboard.git',
+        siteLink: 'https://github.com/TomasRullan/Weather-Dashboard.git'
+    },
+    {
+        id: 4,
+        name: 'Note Taker',
+        image: notetaker,
+        repoLink: 'https://github.com/TomasRullan/note-taker.git',
+        siteLink: 'https://github.com/TomasRullan/note-taker.git'
+    },
+    {
+        id: 5,
+        name: 'ecmmers',
+        image: ecommers,
+        repoLink: 'https://github.com/TomasRullan/E-commers.git',
+        siteLink: 'https://github.com/TomasRullan/E-commers.git'
+    },
+    {
+        id: 6,
+        name: 'blog post',
+        image: blogpost,
+        repoLink: 'https://github.com/TomasRullan/blog-post.git',
+        siteLink: 'https://polar-wildwood-34049-7bb01d304351.herokuapp.com/'
+    }
+];
+
+export default projects;
